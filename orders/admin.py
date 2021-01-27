66b9ab34d13f52bd21b4f4b468c33fd9ea7a5b94
Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>
2021-01-27 19:42:31 -0400

    Gestión de modelos desde el apartado administrativo

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Client, Order, Size, Ingredient, Pizza, Pizza_Ingredient, Drink, Order_Drink, Delivery
+
+admin.site.register(Client)
+admin.site.register(Order)
+admin.site.register(Size)
+admin.site.register(Ingredient)
+admin.site.register(Pizza)
+admin.site.register(Pizza_Ingredient)
+admin.site.register(Drink)
+admin.site.register(Order_Drink)
+admin.site.register(Delivery)
+
 # Register your models here.
