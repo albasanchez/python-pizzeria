e2d36bcdc73d2385f7422b30a133125d4896696f
Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>
2021-01-28 01:09:37 -0400

    GET para pedidos

--- orders/views.py
+++ orders/views.py
@@ -1,18 +1,36 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Drink, Ingredient, Size
 
 import json
 
-def index(request):
-  template = loader.get_template('orders/index.html')
-  items = ['Queso', 'Fruta', 'Chocolate', 'Helado', 'Ropa', 'Zapatos']
-  context = {
-    'int_data': 3,
-    'str_data': 'Hola, esta es una prueba de string',
-    'array_data': items,
-  }
-  return HttpResponse(template.render(context, request))
+def main(request):
+  if request.method == 'POST':
+    return HttpResponse("Test POST")
+  
+  if request.method == 'GET':
+    template = loader.get_template('orders/index.html')
+    ingredients = Ingredient.objects.all()
+    sizes = Size.objects.all()
+    drinks = Drink.objects.all()
 
-def test(request):
-  template = loader.get_template('orders/test.html')
-  return HttpResponse(template.render({}, request))
+    template_ingredients = []
+    template_sizes = []
+    template_drinks = []
+
+    for ingredient in ingredients:
+      template_ingredients.append({'id': ingredient.id, 'name': ingredient.name, 'price': ingredient.price})
+    
+    for size in sizes:
+      template_sizes.append({'id': size.id, 'name': size.name, 'price': size.price})
+    
+    for drink in drinks:
+      template_drinks.append({'id': drink.id, 'name': drink.name, 'price': drink.price})
+
+    context = {
+      'ingredients': template_ingredients,
+      'sizes': template_sizes,
+      'drinks': template_drinks
+    }
+
+    return HttpResponse(template.render(context, request))      
