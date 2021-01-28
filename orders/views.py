372536c6dbf444b7a5fd3b3bfd6a93e3d46f243e
Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>
2021-01-27 20:38:10 -0400

    Templates con Vue.js y Vuetify

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
+from django.template import loader
+
+import json
 
 def index(request):
-  return HttpResponse("Hola UCAB. Usted está en el índice de orders")
+  template = loader.get_template('orders/index.html')
+  items = ['Queso', 'Fruta', 'Chocolate', 'Helado', 'Ropa', 'Zapatos']
+  context = {
+    'int_data': 3,
+    'str_data': 'Hola, esta es una prueba de string',
+    'array_data': items,
+  }
+  return HttpResponse(template.render(context, request))
+
+def test(request):
+  template = loader.get_template('orders/test.html')
+  return HttpResponse(template.render({}, request))
