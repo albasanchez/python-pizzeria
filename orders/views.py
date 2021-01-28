b7215d4da0217a580b959e7953afe82cb7daa072
Miguel Antonio Peña Fraga <mapena.16@est.ucab.edu.ve>
2021-01-28 15:35:56 -0400

    Registro de información del pedido en la base de datos

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse, Http404, JsonResponse
 from django.template import loader
-from .models import Drink, Ingredient, Size
+from .models import Drink, Ingredient, Size, Client, Order, Delivery, Order_Drink, Pizza, Pizza_Ingredient
+from django.db import transaction
+from datetime import date
+
 import json
 
 def main(request):
@@ -81,8 +84,39 @@ def calculateOrder(request):
 
   return JsonResponse(response)
 
+@transaction.atomic
 def registerOrder(request):
-  data = json.loads(request.body)
-  print(data)
-  print(data["order"])
-  return HttpResponse(data["order"])
+  try:
+    data = dict(json.loads(request.body))
+    order = data["order"]
+    
+    pizzas = order["pizzas"]["pizzas"]
+    drinks = order["drinks"]["drinks"]
+    delivery = order["delivery"]
+    client = order["client"]
+
+    #Registro de información del cliente
+    registered_client = Client.objects.create(name=client["name"], last_name=client["last_name"], email=client["email"])
+    
+    #Registro de información de la orden
+    registered_order = Order.objects.create(date=date.today(), price=order["total_order_price"], fk_client=registered_client)
+
+    #Registro de información de delivery
+    registered_delivery = Delivery.objects.create(price=delivery["price"], fk_order=registered_order, direction=delivery["direction"])
+
+    #Registro de información de las bebidas en la orden
+    for drink in drinks:
+      fk_drink = Drink.objects.get(pk=drink["id"])
+      Order_Drink.objects.create(fk_order=registered_order, fk_drink=fk_drink)
+    
+    #Registro de información de las pizzas en la orden
+    for pizza in pizzas:
+      fk_size = Size.objects.get(pk=pizza["size"]["id"])
+      registered_pizza = Pizza.objects.create(price=pizza["total_pizza_price"], fk_size=fk_size, fk_order=registered_order)
+      for ingredient in pizza["ingredients"]:
+        fk_ingredient = Ingredient.objects.get(pk=ingredient["id"])
+        Pizza_Ingredient.objects.create(fk_pizza=registered_pizza, fk_ingredient=fk_ingredient)
+
+    return JsonResponse({"status": 200})
+  except:
+    return JsonResponse({"status": 500})
